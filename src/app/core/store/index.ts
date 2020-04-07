b775fa9f1bf010bb127a19490da7092be9d0b032
Ilya Kabanov <9848333@gmail.com>
2020-04-07 10:57:06 +0200

    console.log actions for debugging

--- src/app/core/store/index.ts
+++ src/app/core/store/index.ts
@@ -3,17 +3,21 @@ import {
   ActionReducerMap,
   createFeatureSelector,
   createSelector,
-  MetaReducer
+  MetaReducer,
 } from '@ngrx/store';
 import { environment } from '../../../environments/environment';
 
-export interface State {
+export interface State {}
 
+export const reducers: ActionReducerMap<State> = {};
+
+export function debug(reducer: ActionReducer<any>): ActionReducer<any> {
+  return function (state, action) {
+    console.log('action', action);
+    return reducer(state, action);
+  };
 }
 
-export const reducers: ActionReducerMap<State> = {
-
-};
-
-
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<State>[] = !environment.production
+  ? [debug]
+  : [];
