b27c248e232a11170808e54f4c82c53f86472435
Ilya Kabanov <9848333@gmail.com>
2020-04-10 10:56:02 +0200

    Clean up root state

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,16 +1,7 @@
-import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer,
-} from '@ngrx/store';
-import { environment } from '../../environments/environment';
+import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 
 export interface State {}
 
 export const reducers: ActionReducerMap<State> = {};
 
-export const metaReducers: MetaReducer<State>[] = !environment.production
-  ? []
-  : [];
+export const metaReducers: MetaReducer<State>[] = [];
