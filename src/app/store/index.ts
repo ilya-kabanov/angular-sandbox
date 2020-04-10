392bbf0174afd1f82e3af1c419245120e9feaee6
Ilya Kabanov <9848333@gmail.com>
2020-04-10 10:39:49 +0200

    Remove debug meta reducer

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -11,13 +11,6 @@ export interface State {}
 
 export const reducers: ActionReducerMap<State> = {};
 
-export function debug(reducer: ActionReducer<any>): ActionReducer<any> {
-  return function (state, action) {
-    console.log('action', action);
-    return reducer(state, action);
-  };
-}
-
 export const metaReducers: MetaReducer<State>[] = !environment.production
-  ? [debug]
+  ? []
   : [];
