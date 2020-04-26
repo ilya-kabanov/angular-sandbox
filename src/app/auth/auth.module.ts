714e3dd3b9b823e22ce8534c93ca574dd655066e
Ilya Kabanov <9848333@gmail.com>
2020-04-26 14:44:20 +0200

    [auth] Login form

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
-import { StoreModule } from '@ngrx/store';
 import * as fromAuth from './reducers/auth.reducer';
-import { EffectsModule } from '@ngrx/effects';
 import { AuthEffects } from './effects/auth.effects';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, AuthRoutingModule, StoreModule.forFeature(fromAuth.authFeatureKey, fromAuth.reducer), EffectsModule.forFeature([AuthEffects])],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    StoreModule.forFeature(fromAuth.authFeatureKey, fromAuth.reducer),
+    EffectsModule.forFeature([AuthEffects]),
+    SharedModule,
+    ReactiveFormsModule,
+  ],
 })
 export class AuthModule {}
--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as AuthActions from '../../actions/auth.actions';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +15,34 @@ import * as AuthActions from '../../actions/auth.actions';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private store: Store) {}
+  loginForm: FormGroup;
 
-  ngOnInit(): void {}
+  get email() {
+    return this.loginForm.get('email');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  constructor(private store: Store, private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
   login(): void {
+    if (!this.loginForm.valid) {
+      return;
+    }
+
     this.store.dispatch(
-      AuthActions.login({ email: 'abc@example.com', password: 'password' })
+      AuthActions.login({
+        email: this.email.value,
+        password: this.password.value,
+      })
     );
   }
 }
